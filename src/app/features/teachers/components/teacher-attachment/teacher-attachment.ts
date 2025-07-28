1801c3e69134a73715846fb41caf25e7a89f165b
Eslam Heikal <eheikal.sw@gmail.com>
2025-07-28 20:30:34 +0300

    handle delete teacher attachment

--- src/app/features/teachers/components/teacher-attachment/teacher-attachment.ts
+++ src/app/features/teachers/components/teacher-attachment/teacher-attachment.ts
@@ -8,10 +8,13 @@ import { Attachment } from '../../../../core/models/attachment.model';
 import { TeacherService } from '../../../../core/services/teacher.service';
 import { AttachmentHelper } from '../../../../core/helpers/attachmet-helper';
 import { DataHelper } from '../../../../core/helpers/data.helper';
+import { DeleteConfirmation } from '../../../shared/components/delete-confirmation/delete-confirmation';
+import { LoaderService } from '../../../shared/services/loader.service';
+import { ToastService } from '../../../shared/services/toast.service';
 
 @Component({
   selector: 'app-teacher-attachment',
-  imports: [CommonModule, DeleteIconButton, DownloadIconButton, ViewIconButton],
+  imports: [CommonModule, DeleteIconButton, DownloadIconButton, ViewIconButton, DeleteConfirmation],
   templateUrl: './teacher-attachment.html',
   styleUrl: './teacher-attachment.scss'
 })
@@ -19,8 +22,14 @@ export class TeacherAttachmentComponent {
 
   @Input() teacher!: Teacher;
 
+  deleteAttachment!: Attachment;
+  showDelete: boolean = false;
+  deleteMessage: string = 'هل أنت متأكد من حذف هذا المرفق؟';
+
   //#region Services
   private teacherService = inject(TeacherService);
+  private loaderService = inject(LoaderService);
+  private toastr = inject(ToastService);
   //#endregion
 
 
@@ -39,8 +48,26 @@ export class TeacherAttachmentComponent {
     return AttachmentHelper.getIcon(attachment.fileName);
   }
 
-  onDeleteAttachment(attachmentId: number) {
-    // console.log(attachment);
+  onDeleteAttachment(attachment: Attachment) {
+    this.deleteAttachment = attachment;
+    this.deleteMessage = `هل أنت متأكد من حذف هذا المرفق <b>${attachment.displayName}</b> ؟`;
+    this.showDelete = true;
+  }
+
+  onDeleteConfirm() {
+    this.loaderService.show();
+    this.teacherService.deleteAttachment(this.deleteAttachment.id).subscribe(res => {
+      if (res) {
+        this.showDelete = false;
+        this.teacher.attachments = this.teacher.attachments.filter(a => a.id !== this.deleteAttachment.id);
+        this.toastr.showSuccess('تم حذف المرفق بنجاح');
+      }
+    },_ => {},
+    () => this.loaderService.hide());
+  }
+
+  onDeleteCancel() {
+    this.showDelete = false;
   }
 
   displayDate(date: string) {
